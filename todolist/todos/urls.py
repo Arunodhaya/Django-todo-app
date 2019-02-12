3f89f74d8e42e2dedd6be26abe7e194ac18ee434
Arunodhaya <arunarajagopal98@gmail.com>
2019-02-12 13:03:19 +0530

    first commit

---
+++ todolist/todos/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index,name='index'),
+    url(r'^details/(?P<id>\w{0,50})/$', views.details),
+    url(r'^add', views.add, name='add'),
+    
+] 
